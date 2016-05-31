3d000a40c7b4f85f8c5aeb172b37a9cef594c4d8
joserprieto <jose@joserprieto.es>
2016-05-31 15:01:17 +0200

    Home page in process
    DONE:
    - Main Sidebar Menu
    - Home carousel responsive; with Swiper jQuery plugin
    - "Loader" of home carousel.
    - Shadows of global sections has to be background imgs for not make vertical scrolling
    - Section Presentation "Arte et Marte"
    - Tags h-N responsives
    - Material Design Icons sizes
    - Paragraphs with responsive text-size
    - Work in news items / news section.
    
    TODO:
    - End the news items.
    - Section "Groups".
    - Section "Patrocinators".
    - Footer.
    - Two tipe of Parallax, with two heights.
    - Parallax will be responsive?

--- src/static/assets/js/init.js
+++ src/static/assets/js/init.js
@@ -19,10 +19,14 @@ $(document).ready(function() {
      */
 
     if ($('body').hasClass('home')) {
-        var time = 7, // time between slides, in seconds
+        var timeBetweenSlides, // time between slides, in seconds
+            timeElapsed,// time transcurred, in ms
+            timeBetweenIntervals,// time between interval calls, in ms
+            previousTime,
+            nowTime,
             progressBar,
             bar,
-            elem = $('.home.top-carousel.swiper-container'),
+            elem,
             isPause,
             tick,
             percentTime,
@@ -49,13 +53,20 @@ $(document).ready(function() {
                     onTouchEnd: continueAfterDragging
                 }
             );
+        // @TODO: change time for precision; to take 7 seconds, not depended of timeOut.
         //Init progressBar where elem is $("#owl-demo")
         function initProgressBar(){
-            //elem = _elem;
-            //build progress bar elements
+            //reset timer
+            timeBetweenSlides = 8;
+            timeElapsed = 0;
+            timeBetweenIntervals = 10;
+            isPause = false;
+            elem = $('.home.top-carousel.swiper-container');
+
             buildProgressBar();
             //start counting
             start();
+
         }
         //create div#progressBar and div#bar then prepend to $("#owl-demo")
         function buildProgressBar(){
@@ -75,24 +86,38 @@ $(document).ready(function() {
         }
         function start() {
             //reset timer
-            percentTime = 0;
+            timeElapsed = 0;
             isPause = false;
-            //run interval every 0.01 second
-            tick = setInterval(interval, 10);
+            // get the "now" time:
+            nowTime = new Date();
+            previousTime = nowTime;
+            //run interval every "timeBetweenIntervals" miliseconds
+            tick = setInterval(interval, timeBetweenIntervals);
         };
         function interval() {
             if(isPause === false){
-                percentTime += 1 / time;
+                var diffTime,
+                    msBetweenSlides;
+                msBetweenSlides = timeBetweenSlides * 1000;
+                nowTime = new Date();
+                diffTime = nowTime - previousTime;
+                previousTime = nowTime;
+                timeElapsed += diffTime;
+                percentTime = (timeElapsed / msBetweenSlides) * 100;
                 bar.css({
                     width: percentTime+"%"
                 });
-                //if percentTime is equal or greater than 100
-                if(percentTime >= 100){
-                    //slide to next item
-                    homeTopSwiperBanner.slideNext();
-
+                // if timeElapsed is equal or greater than timeBetweenSlides (msBetweenSlides)
+                if (timeElapsed >= msBetweenSlides) {
+                    stopInterval();
                 }
             }
+        }
+        function stopInterval() {
+            //clear interval
+            clearTimeout(tick);
+            //slide to next item
+            homeTopSwiperBanner.slideNext();
         }
         //pause while dragging
         function pauseOnDragging(){
@@ -104,8 +129,6 @@ $(document).ready(function() {
         }
         //moved callback
         function moved(){
-            //clear interval
-            clearTimeout(tick);
             //start again
             start();
         }
